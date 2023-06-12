56ee513f4f8045e0dbba60f4c66dada72006a508
Kawser420 <muhammadkawser202@gmail.com>
2023-06-12 11:59:13 +0600

    some react code updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,35 +9,41 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        console.log('product load before fetch')
         fetch('products.json')
             .then(res => res.json())
-            .then(data => {
-                setProducts(data);
-                // console.log('product loaded')
-            })
+            .then(data => setProducts(data));
     }, []);
 
     useEffect( () => {
-        console.log('Local storage first Line', products)
         const storedCart = getShoppingCart();
-        const saveCart = [];
-        for(const id in storedCart){
+        const savedCart = [];
+        for(const id in storedCart) {
             const addedProduct = products.find(product => product.id === id);
             if(addedProduct){
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
-                saveCart.push(addedProduct);
+                savedCart.push(addedProduct);
             }
         }
-        setCart(saveCart);
-        // console.log('Local storage finished')
-    }, [products]) 
+        setCart(savedCart);
+    }, [products]);
+   
 
-    const handleToAddCart = (product) => {
-        const newCart = [...cart, product];
+    const handleToAddCart = (selectedProduct) => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
 
     }
 
