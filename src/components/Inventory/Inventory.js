fcf9316ab9586ccdd7922acdc1ddb1792eadd0b1
Kawser420 <muhammadkawser202@gmail.com>
2023-08-01 03:17:41 +0600

    add some file add

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            This in Inventory
+        </div>
+    );
+};
+
+export default Inventory;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,7 +5,7 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = ({product, handleToAddCart}) => {
     // const {product, handleToAddCart} = props;
-    const {name, seller, img, price, rattings} = product;
+    const {name, seller, img, price, ratings} = product;
 
     return (
         <div className='product'>
@@ -14,7 +14,7 @@ const Product = ({product, handleToAddCart}) => {
             <p className="product-name">{name}</p>
             <p>Price: ${price}</p>
             <p>Seller: {seller}</p>
-            <p><small>Ratting: {rattings} star</small></p>
+            <p><small>Ratting: {ratings} star</small></p>
             </div>
             <button className='btn-name' onClick={() => handleToAddCart(product)}>
                 <p className='btn-cart-name'>Add to Cart</p>
